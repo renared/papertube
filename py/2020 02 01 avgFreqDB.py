368c9e9196ab29d7e1bf9f1de482bee8027b2d26
renared <yann.bourdin@gmail.com>
2020-02-01 19:41:46 +0100

    &&

---
+++ py/2020 02 01 avgFreqDB.py
@@ -0,0 +1,8 @@
+t,f=avgFreqDB(dureeHold=14,commentaire="var longueur")
+plt.plot(t,f,label="10 s")
+t,f=avgFreqDB(dureeHold=20,commentaire="var longueur")
+plt.plot(t,f,label="20 s")
+t,f=avgFreqDB(dureeHold=30,commentaire="var longueur")
+plt.plot(t,f,label="30 s")
+plt.legend()
+plt.show()
