50ed4d4d5a3d7fd2a8c53c05801f7a6c27d12a36
Yann Bourdin <yann.bourdin@grenoble-inp.org>
2020-02-06 00:17:10 +0100

    avgFreqDB now returns les écarts-types, exemple dutilisation

--- py/2020 02 06a.py
+++ py/2020 02 06a.py
@@ -3,7 +3,7 @@
 t,f,f_err=avgFreqDB(dureeHold=10,commentaire="var dureeHold")
 t,f=t[:-1],f[:-1]
 T = 1/f
-err = T/(1+f_err[:-1]*T)
+err = T/(1+f_err[:-1]*T)    # calcul de l'erreur sur T à partir de l'erreur sur f
 t2,T2,a1,b1,r_squared = regressionT1(t,f)
 # t2,f2,x0,a,r2=regressionPowF(t,f,power=p)
 print("coeffs a1=",a1,"; r²=",r_squared)
